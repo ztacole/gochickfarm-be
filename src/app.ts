ea07d26e153c565e6fc88371fc0e12911052902f
Jeki <ahmadzaqi490@gmail.com>
2025-10-23 12:47:10 +0700

    refactor(app): separate route for mobile and web

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,36 @@ import { errorHandler } from './middleware/error.middleware';
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+import authMobileRoute from './modules/auth/auth.route.mobile';
+import authWebRoute from './modules/auth/auth.route.web';
+import animalMobileRoute from './modules/animal/animal.route.mobile';
+import animalWebRoute from './modules/animal/animal.route.web';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Main
+/*
+|--------------------------------------------------------------------------
+| Mobile Routes
+|--------------------------------------------------------------------------
+*/
+const mobilePrefix = '/api/v1/mobile';
+app.use(mobilePrefix + '/auth', authMobileRoute);
+app.use(mobilePrefix + '/animals', animalMobileRoute);
 
-//
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+const webPrefix = '/api/v1/web';
+app.use(webPrefix + '/auth', authWebRoute);
+app.use(webPrefix + '/animals', animalWebRoute);
 
+// Error Handler Middleware
 app.use(errorHandler)
 
 export default app;
